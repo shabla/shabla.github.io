bbdac2e0eb41a09b00ce1c602dbd713d0eb384b0
Mathieu Collinge <shabla@gmail.com>
2019-04-04 23:46:21 -0400

    first build

---
+++ precache-manifest.d3dc8d56f13e22a0691984bfa37f5b67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f83bb6f391cc7000eb7f",
+    "url": "/static/js/main.af291bd3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "00642f30b2a5e9e705aa",
+    "url": "/static/css/2.5c54d749.chunk.css"
+  },
+  {
+    "revision": "00642f30b2a5e9e705aa",
+    "url": "/static/js/2.d985a3a6.chunk.js"
+  },
+  {
+    "revision": "181a251f16e1625c3886884da8c2abb8",
+    "url": "/index.html"
+  }
+];
