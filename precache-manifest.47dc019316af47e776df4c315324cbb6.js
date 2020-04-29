b7275e9af7088f807b7a469655e6249c135f0e49
Mathieu Collinge <shabla@gmail.com>
2020-04-28 23:14:05 -0400

    replaced with testing stuff

---
+++ precache-manifest.47dc019316af47e776df4c315324cbb6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd355afd7bf1170b555a704b2ea6f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae211848c1dd747eeba6",
+    "url": "/static/css/2.f2cd2c08.chunk.css"
+  },
+  {
+    "revision": "be5eae38888f00268118",
+    "url": "/static/css/main.f5fc5dfc.chunk.css"
+  },
+  {
+    "revision": "ae211848c1dd747eeba6",
+    "url": "/static/js/2.f73f40db.chunk.js"
+  },
+  {
+    "revision": "77b8b824c7f7019f6f1137f3f22740c8",
+    "url": "/static/js/2.f73f40db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be5eae38888f00268118",
+    "url": "/static/js/main.020ac139.chunk.js"
+  },
+  {
+    "revision": "df6b99a52a8e5e0ef6be",
+    "url": "/static/js/runtime-main.c3eeba1a.js"
+  },
+  {
+    "revision": "57f4cfcdad20e06b62bdbb93ecc8faae",
+    "url": "/static/media/char.57f4cfcd.webp"
+  },
+  {
+    "revision": "fe1180da4458bd77e1e4fda5ce03b463",
+    "url": "/static/media/grass.fe1180da.png"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "9d937c2c5d27cd31bbaeca61c47a137f",
+    "url": "/static/media/portrait.9d937c2c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3dc8d56f13e22a0691984bfa37f5b67.js"
+  "/precache-manifest.47dc019316af47e776df4c315324cbb6.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
