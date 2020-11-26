4a2309672090f6a2fd26ca39e9825a32914dbb6c
Mathieu Collinge <shabla@gmail.com>
2020-11-26 09:24:07 -0500

    added fire app

---
+++ fire/precache-manifest.73a15b270fe8dc03b642dda9f7c65a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73185fea192603f4274d62050279787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56dfc0ea27e4ff761a6c",
+    "url": "/static/css/main.49010776.chunk.css"
+  },
+  {
+    "revision": "db24817de461c0273e00",
+    "url": "/static/js/2.0ff21ced.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/static/js/2.0ff21ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dfc0ea27e4ff761a6c",
+    "url": "/static/js/main.758c3bba.chunk.js"
+  },
+  {
+    "revision": "4b959cc73081a354f197",
+    "url": "/static/js/runtime-main.3a0d2c85.js"
+  }
+]);
